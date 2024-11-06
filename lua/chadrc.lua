a8c6cc50a2e6c214bf3713dfe7bfc58ca405d1c3
IRTIDEATH <enderjhass@gmail.com>
2024-11-06 22:53:27 +0700

    upd: change theme to aquarium

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,13 +6,13 @@
 local M = {}
 
 M.base46 = {
-  theme = "ashes", -- default theme
+  theme = "aquarium", -- default theme
   hl_add = {},
   hl_override = {},
   integrations = {},
   changed_themes = {},
   transparency = false,
-  theme_toggle = { "ashes", "one_light" },
+  theme_toggle = { "aquarium", "one_light" },
 }
 
 M.ui = {
